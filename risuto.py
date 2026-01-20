9fd4239214286d78c9d1714aafbe804a03406a4e
nanikaaa <238958904+sorakaradenkyuuga@users.noreply.github.com>
2026-01-20 23:25:17 +0900

    risuto.pyの作成

---
+++ risuto.py
@@ -0,0 +1,9 @@
+#リスト関連の学習
+list_a=["a","b","c","d","e","f","g","h","i","j"]
+print(list_a[:3])   #a,b,c
+print(list_a[3:6])  #d,e,f
+print(list_a[:])  #全部表示
+print(list_a[::2])  #2つ飛ばしで表示
+print(list_a[::-1])  #逆順で表示
+print(list_a[5::2])# f,h,j
+print(list_a[2:8:2]) #c,e,g
